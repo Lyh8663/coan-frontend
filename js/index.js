b8c63a0f68b02f7dc36ea212418ef118ae43abe4
zhuluo-lin <1005772685@qq.com>
2023-03-02 11:54:49 +0800

    lyh03021154

--- js/index.js
+++ js/index.js
@@ -1,37 +1,8 @@
-let menuItem0 = document.getElementById("menuItem0");
-		let menuItem1 = document.getElementById("menuItem1");
-		let menuItem2 = document.getElementById("menuItem2");
-		let menuItem3 = document.getElementById("menuItem3");
-		menuItem0.onmouseover = function(){
-			menuItem0.className = 'menuItemChosen';
-		}
-		menuItem0.onmouseout = function(){
-			menuItem0.className = 'menuItem';
-		}
-		menuItem1.onmouseover = function(){
-			menuItem1.className = 'menuItemChosen';
-		}
-		menuItem1.onmouseout = function(){
-			menuItem1.className = 'menuItem';
-		}
-		menuItem2.onmouseover = function(){
-			menuItem2.className = 'menuItemChosen';
-		}
-		menuItem2.onmouseout = function(){
-			menuItem2.className = 'menuItem';
-		}
-		menuItem3.onmouseover = function(){
-			menuItem3.className = 'menuItemChosen';
-		}
-		menuItem3.onmouseout = function(){
-			menuItem3.className = 'menuItem';
-		}
-		
-		//给主按钮添加鼠标经过效果
-		let entryButton = document.getElementById("entryButton");
-		entryButton.onmouseover = function(){
-			entryButton.className = 'entryButtonChosen';
-		}
-		entryButton.onmouseout = function(){
-			entryButton.className = 'entryButton';
-		}
+//给主按钮添加鼠标经过效果
+let entryButton = document.getElementById("entryButton");
+	entryButton.onmouseover = function(){
+	entryButton.className = 'entryButtonChosen';
+}
+entryButton.onmouseout = function(){
+	entryButton.className = 'entryButton';
+}
---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+let menuItem0 = document.getElementById("menuItem0");
+let menuItem1 = document.getElementById("menuItem1");
+let menuItem2 = document.getElementById("menuItem2");
+let menuItem3 = document.getElementById("menuItem3");
+menuItem0.onmouseover = function(){
+	menuItem0.className = 'menuItemChosen';
+}
+menuItem0.onmouseout = function(){
+	menuItem0.className = 'menuItem';
+}
+menuItem1.onmouseover = function(){
+	menuItem1.className = 'menuItemChosen';
+}
+menuItem1.onmouseout = function(){
+	menuItem1.className = 'menuItem';
+}
+menuItem2.onmouseover = function(){
+	menuItem2.className = 'menuItemChosen';
+}
+menuItem2.onmouseout = function(){
+	menuItem2.className = 'menuItem';
+}
+menuItem3.onmouseover = function(){
+	menuItem3.className = 'menuItemChosen';
+}
+menuItem3.onmouseout = function(){
+	menuItem3.className = 'menuItem';
+}
+		
