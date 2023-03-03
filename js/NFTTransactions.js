aa5dc7c2781bc433016b2ee99a23cf12e36c300c
zhuluo-lin <1005772685@qq.com>
2023-03-03 15:40:53 +0800

    lyh03031540

---
+++ js/NFTTransactions.js
@@ -0,0 +1,15 @@
+//选择栏的控制
+var sel_acc = document.getElementsByClassName("sel-accordion");
+var sel_i;
+
+for (sel_i = 0; sel_i < sel_acc.length; sel_i++) {
+  sel_acc[sel_i].addEventListener("click", function() {
+    this.classList.toggle("sel-active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
