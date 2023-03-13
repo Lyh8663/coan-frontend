6f03879cd58b97f4ff8f6ad4711e4a2b20c3b1ab
zhuluo-lin <1005772685@qq.com>
2023-03-13 11:26:10 +0800

    lyh03131126

---
+++ js/LoginAndRegist.js
@@ -0,0 +1,24 @@
+const loginsec=document.querySelector('.login-section')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginsec.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginsec.classList.remove('active')
+})
+function sendEmailCode(){
+	var email = document.getElementById('email').value; // 获取指定 id 的内容
+	console.log("用户登录的email为" + email);
+	//以后要调用一下后台的发送验证码接口
+} 
+function login(){
+	console.log("登录");
+	var email = document.getElementById('email').value;
+	var code = document.getElementById('code').value;
+	if(email=='123@qq.com'&&code=='123456'){//后门
+		window.open("index.html");
+		window.close("LoginAndRegist.html");
+		console.log("登录成功");
+	}
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 //给主按钮添加鼠标经过效果
-let entryButton = document.getElementById("entryButton");
+let entryButton = document.getElementById("entryButton1");
 	entryButton.onmouseover = function(){
 	entryButton.className = 'entryButtonChosen';
 }
 entryButton.onmouseout = function(){
-	entryButton.className = 'entryButton';
+	entryButton.className = 'entryButton1';
 }
