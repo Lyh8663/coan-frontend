64ee02fa31c663d0f7599a24e8ddefdc658f0c72
zhuluo-lin <1005772685@qq.com>
2023-03-23 11:46:08 +0800

    lyh03231146

---
+++ js/Self-infomation.js
@@ -0,0 +1,13 @@
+// window.onload = function(){
+// 	//修改用户名
+// 	let username = window.sessionStorage.getItem("username");
+// 	let usernameArea = document.getElementById("name");
+// 	usernameArea.innerHTML = username;
+	
+// 	//修改id
+// 	let userid = window.sessionStorage.getItem("userId");
+// 	let useridArea = document.getElementById("id");
+// 	useridArea.innerHTML = "uID: " + userid;
+	
+	
+// }
--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-window.onload = function(){
-	console.log(window.sessionStorage.getItem("userInfo"));
-}
-
 //给主按钮添加鼠标经过效果
 let entryButton1 = document.getElementById("entryButton1");
 	entryButton1.onmouseover = function(){
